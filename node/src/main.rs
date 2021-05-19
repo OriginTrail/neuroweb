23c068f18093437f5b9e6616a3604829642314e3
Milos Kotlar <kotlarmilos@gmail.com>
2021-05-19 21:02:45 +0200

    update

--- node/src/main.rs
+++ node/src/main.rs
@@ -1,10 +1,13 @@
 #![warn(missing_docs)]
+#![warn(unused_extern_crates)]
 
 mod chain_spec;
 #[macro_use]
 mod service;
 mod cli;
 mod command;
+mod inherents;
+mod rpc;
 
 fn main() -> sc_cli::Result<()> {
 	command::run()
