52868b23b44f168d7b3bd49bfca5aae437367d9f
Milos Kotlar <kotlarmilos@gmail.com>
2021-05-19 19:07:35 +0200

    update

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -8,8 +8,9 @@ use serde::{Deserialize, Serialize};
 use sp_core::{H160, U256, Pair, Public, sr25519};
 use sp_runtime::{
 	traits::{BlakeTwo256, Hash, IdentifyAccount, Verify},
-	Perbill,
+	Perbill, Percent
 };
+
 
 use pallet_evm::GenesisAccount;
 use std::str::FromStr;
@@ -202,7 +203,7 @@ fn testnet_genesis(
 			stakers: stakers.clone(),
 			inflation_config,
 		},
-		pallet_author_slot_filter: AuthorFilterConfig { eligible_ratio: 50 },
+		pallet_author_slot_filter: AuthorFilterConfig { eligible_ratio: Percent::from_percent(50), },
 		pallet_author_mapping: AuthorMappingConfig {
 			// Pretty hacky. We just set the first staker to use alice's session keys.
 			// Maybe this is the moment we should finally make the `--alice` flags make sense.
