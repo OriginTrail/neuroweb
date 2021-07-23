292b9262211062c35d24c5c2930aa6f6601904b6
NZT48 <nikolaztodorovic26@gmail.com>
2021-07-23 13:52:08 +0000

    Runtime version incremented to 104 for testnet runtime ugprade

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -132,7 +132,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("origintrail-parachain"),
 	impl_name: create_runtime_str!("origintrail-parachain"),
 	authoring_version: 1,
-    spec_version: 103,
+    spec_version: 104,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
