0d885d9c9ebf9da857a59d29f5e47801afffe72d
NZT48 <nikolaztodorovic26@gmail.com>
2022-06-14 22:25:51 +0200

    Revert "Increment versions for 1.0.1 runtime upgrade"
    This reverts commit 9ec4230c7c8f23fc703941f4c00802f3a59850ba.

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -171,7 +171,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("origintrail-parachain"),
 	impl_name: create_runtime_str!("origintrail-parachain"),
 	authoring_version: 1,
-	spec_version: 101,
+	spec_version: 100,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
