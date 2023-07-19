2fd92931d91eb89237a28df279d3deb4e2d4e1d8
NZT48 <nikolaztodorovic26@gmail.com>
2023-07-19 01:32:18 +0000

    Bump runtime version for Alphanet to 113

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -250,7 +250,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("origintrail-parachain"),
     impl_name: create_runtime_str!("origintrail-parachain"),
     authoring_version: 1,
-    spec_version: 112,
+    spec_version: 113,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
