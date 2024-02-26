39388ff96ff6397da20c6a9523285c4cf6ed277b
NZT48 <nikolaztodorovic26@gmail.com>
2024-02-26 16:32:08 +0100

    Bump version to 1.3.0

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -250,7 +250,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("origintrail-parachain"),
     impl_name: create_runtime_str!("neuroweb"),
     authoring_version: 1,
-    spec_version: 122,
+    spec_version: 130,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
