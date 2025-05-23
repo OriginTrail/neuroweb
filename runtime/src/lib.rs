ad3ccf419e303f1e75512d4755c9eaaaa6d8875f
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2025-05-23 20:57:51 +0200

    Update spec_version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -142,7 +142,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("origintrail-parachain"),
     impl_name: create_runtime_str!("neuroweb"),
     authoring_version: 1,
-    spec_version: 137,
+    spec_version: 138,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
