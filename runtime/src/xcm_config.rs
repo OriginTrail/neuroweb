388ddee4afefdb0a63ee38e232fe79a907355883
claravanstaden <claravanstaden64@gmail.com>
2025-09-01 20:31:52 +0200

    Revert "use pallet assets"
    This reverts commit e87e6230c3bcf41585870f25b1bf7d942dfe099e.

--- runtime/src/xcm_config.rs
+++ runtime/src/xcm_config.rs
@@ -97,14 +97,17 @@ pub type LocationToAccountId = (
 pub type NativeAssetTransactor =
     FungibleAdapter<Balances, IsConcrete<TokenLocation>, LocationToAccountId, AccountId, ()>;
 
-pub type BridgedLocalAssetTransactor = FungiblesAdapter<
-    crate::Assets,
-    // Use pallet assets asset when receiving TRAC from Ethereum
-    LocalAssetWhenReceiving<EthereumCurrencyLocation>,
+pub type BridgedLocalAssetTransactor = FungibleAdapter<
+    // Use this currency:
+    Balances,
+    // Use this currency when it is a fungible asset matching the given location or name:
+    IsConcrete<EthereumCurrencyLocation>,
+    // Convert an XCM Location into a local account id:
     LocationToAccountId,
+    // Our chain's account ID type (we can't get away without mentioning it explicitly):
     AccountId,
-    NoChecking,
-    CheckingAccount,
+    // We don't track any teleports.
+    (),
 >;
 
 pub type ForeignAssetTransactor = FungiblesAdapter<
@@ -346,26 +349,8 @@ impl Contains<RuntimeCall> for SafeCallFilter {
     }
 }
 
-pub struct LocalAssetWhenReceiving<AssetLocation>(PhantomData<AssetLocation>);
-
-impl<AssetLocation> MatchesFungibles<u128, u128> for LocalAssetWhenReceiving<AssetLocation>
-where
-    AssetLocation: Get<Location>,
-{
-    fn matches_fungibles(a: &Asset) -> Result<(u128, u128), MatchError> {
-        let AssetId(asset_location) = &a.id;
-        let expected_location = AssetLocation::get();
-
-        if *asset_location == expected_location {
-            if let Fungible(amount) = a.fun {
-                return Ok((crate::assets::LOCAL_TRAC_ASSET_ID, amount));
-            }
-        }
-
-        Err(MatchError::AssetNotHandled)
-    }
-}
-
+/// Matches foreign assets from a given origin.
+/// Foreign assets are assets bridged from other consensus systems. i.e parents > 1.
 pub struct IsDOTFrom<Origin>(PhantomData<Origin>);
 
 impl<Origin> MatchesFungibles<Location, u128> for IsDOTFrom<Origin>
